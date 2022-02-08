b95b741efd2abce0b7606888d18244de5c525597
ArchLeaders <archparliament@gmail.com>
2022-02-07 23:34:11 -0800

    New (old) caching system

--- io/system.py
+++ io/system.py
@@ -3,68 +3,23 @@ import subprocess
 from .oead import OpenOead
 from pathlib import Path
 
-class CachedActor:
-    """Meta data for cached actors"""
-
-    name = ''
-    """Name of the actor"""
-
-    display_name = ''
-    """In game name of the actor"""
-
-    dae_file = ''
-    """Full path to the actors collada file"""
-
-    folder = ''
-    """Model folder name, aka bfres name"""
-
-    unit = ''
-    """Model unit name, commonly shares the actor name"""
-
-class Cache:
+class Data:
     """Cached actors"""
 
     data_dir = json.loads(Path(f'.\\config.json').read_text())['data_dir']
     """Path to the storage directory"""
 
-    exported: dict = {}
+    exported: dict = json.loads(Path(f'{data_dir}\\exported.json').read_text())
     """Dictionary of exported actors"""
 
-    cache: dict = {}
+    cache: dict = json.loads(Path(f'{data_dir}\\cache.json').read_text())
     """Dictionary of cached actors"""
 
-    def register():
-        """Sets the current export and cache data to the ExportList and CacheList class."""
-
-        sets = [ 'exported', 'cache' ]
-
-        for file in sets:
-            file_data = json.loads(Path(f'{Cache.data_dir}\\{file}.json').read_text())
-            data: dict = {}
-
-            for key, value in file_data:
-                data[key] = {
-                    CachedActor(
-                        name=key,
-                        display_name=value['DisplayName'],
-                        folder=value['BfresName'],
-                        unit=value['ModelName'],
-                        dae_file=f"{Cache.data_dir}\\{file}\\{value['BfresName']}\\{value['ModelName']}.dae"
-                    )
-                }
-
-            if file == 'exported':
-                exported = data
-            elif file == 'cache':
-                cache = data
-
-    def add_actor(actorname, mod_dir) -> CachedActor:
+    def cache_actor(actorname, mod_dir) -> dict:
         """Caches an actor from an actorpack file"""
 
-        cache_data = json.loads(Path(f'{Cache.data_dir}\\cache.json').read_text())
-
         # Check for existing entry
-        if actorname.name in cache_data:
+        if actorname.name in data.cache:
             return
 
         # Look for actorpack
@@ -82,7 +37,7 @@ class Cache:
                     model_data = modellist.lists["ModelData"].lists["ModelData_0"]
                     unit_name = model_data.lists["Unit"].objects["Unit_0"].params["UnitName"]
                     folder_name = model_data.objects["Base"].params["Folder"]
-                    cache_data[actorname] = {
+                    data.cache[actorname] = {
                         "DisplayName": actorname.name,
                         "BfresName": folder_name,
                         "ModelName": unit_name,
@@ -95,7 +50,7 @@ class Cache:
 
         subprocess.run(
             args=[
-                f'"{Cache.data_dir}\\lib\\SbfresExtracter.exe"',
+                f'"{data.data_dir}\\lib\\SbfresExtracter.exe"',
                 f'"{mod_dir}\\content\\Model\\{folder_name}.sbfres"',
                 f'"{mod_dir}\\content\\Model\\{folder_name}Tex1.sbfres"',
                 f'"{folder_name}"'
@@ -103,4 +58,4 @@ class Cache:
             capture_output=True
         )
 
-        Path(f'{Cache.data_dir}\\cache.json').write_text(json.dumps(cache_data, indent=4))
+        Path(f'{data.data_dir}\\cache.json').write_text(json.dumps(data.cache, indent=4))
